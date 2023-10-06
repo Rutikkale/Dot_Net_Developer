0e5d7059f62b223965fa2979b88cd963199e7dae
Rutikkale <rutikkale17@gmail.com>
2023-10-06 16:31:37 +0530

    increment_and_decrement_operators

---
+++ C#/Increment_and_decrement_operators/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Increment_and_decrement_operators
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //int a = 10;
+            //Console.WriteLine(++a); //pre-increment
+            //Console.ReadLine();
+
+            //o/p: 11
+
+
+            //int a = 10;
+            //Console.WriteLine(a++); //post-increment
+            //Console.WriteLine(a);
+            //Console.ReadLine();
+
+            //o/p: 10
+            //     11
+
+            //int a = 20;
+            //Console.WriteLine(--a);//pre-decrement
+            //Console.ReadLine();
+
+            //o/p: 19
+
+
+            int a = 20;
+            Console.WriteLine(a++); //post-decrement
+            Console.WriteLine(a);
+            Console.ReadLine();
+
+            //o/p:20
+            //    21
+                
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
