cdcfd37f4f14a93a36152584e80227190754158e
Rutikkale <rutikkale17@gmail.com>
2023-10-06 13:26:45 +0530

    logical_operators

---
+++ C#/Logical Operators/Logical_Operators/Program.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logical_Operators
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //AND logical operators
+
+            // condition True && False
+
+            //int a = 50;
+            //int b = 40;
+
+            //bool c = (a>b) && (b>a);
+            //Console.WriteLine(c);
+            //Console.ReadLine();
+
+            // o/p: False
+
+
+            // condition True && True
+
+            //int a = 50;
+            //int b = 40;
+
+            //bool c = (a > b) && (b < a);
+            //Console.WriteLine(c);
+            //Console.ReadLine();
+
+            //o/p: True
+
+            // condition False && False
+
+            //int a = 50;
+            //int b = 40;
+
+            //bool c = (a<b) && (b>a);
+            //Console.WriteLine(c);
+            //Console.ReadLine();
+
+            //o/p: False
+
+
+
+            // OR logical operators
+
+            //condition True || True
+
+            //int a = 50;
+            //int b = 40;
+
+            //bool c = (a > b) || (b < a);
+            //Console.WriteLine(c);
+            //Console.ReadLine();
+
+            //o/p: True
+
+
+            // condition True || False
+
+            //int a = 50;
+            //int b = 40;
+
+            //bool c = (a>b) || (b>a);
+            //Console.WriteLine(c);
+            //Console.ReadLine();
+
+            //o/p: True
+
+            // condition False || False
+
+            int a = 50;
+            int b = 40;
+
+            bool c = (a<b) || (b>a);
+            Console.WriteLine(c);
+            Console.ReadLine();
+           
+            //o/p: False
+
+         
+
+
+
+        }
+    }
+}
