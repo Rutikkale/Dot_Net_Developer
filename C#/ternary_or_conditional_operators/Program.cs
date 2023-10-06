c20b7c091d8c5d0d76bbbd0da66ed35cce0aed3b
Rutikkale <rutikkale17@gmail.com>
2023-10-06 17:20:11 +0530

    ternary_or_conditional_operators

---
+++ C#/ternary_or_conditional_operators/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ternary_or_conditional_operators
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // 1)Example
+
+            //int a = 10;
+            //string b = (a > 20) ? "A is greater" : "A is lesser";
+            //Console.WriteLine(b);
+            //Console.ReadLine();
+
+            //o/p: A is lesser
+
+            //int a = 50;
+            //String b = (a > 20) ? "A is greater" : "A is lesser";
+            //Console.WriteLine(b);
+            //Console.ReadLine();
+
+            //o/p: A is greater
+
+
+
+            // 2)Example
+
+            int a = int.Parse(Console.ReadLine());
+            string b = (a >= 18) ? "You are eligible for vote" : "You are not eligible for vote";
+            Console.WriteLine(b);
+            Console.ReadLine();
+
+            //o/p: 20
+            //     You are eligible for vote
+
+
+
+        }
+    }
+}
