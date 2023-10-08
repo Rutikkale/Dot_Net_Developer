b609cc34aca4a93f312e86bb821823481ce7db4a
Rutikkale <rutikkale17@gmail.com>
2023-10-08 16:21:51 +0530

    precedence_of_operators

---
+++ C#/Precedence_of_operators/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Precedence_of_operators
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 8 / 2 - (3 + 2) * 2;
+
+            // 8/2-5*2
+            //4-5*2
+            //4-10
+            //-6
+
+            Console.WriteLine(a);
+            Console.ReadLine();
+            
+            // highest level-- (),/,*,+,-
+            // BODMAS Rules6
+
+
+            //o/p: -6
+        }
+    }
+}
