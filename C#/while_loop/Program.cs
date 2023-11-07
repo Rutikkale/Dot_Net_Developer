a6d2ae92eef5b66b0143dc80606f21fd860c64b5
Rutikkale <rutikkale17@gmail.com>
2023-11-07 16:24:05 +0530

    while loop

---
+++ C#/while_loop/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace while_loop
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your number");
+            int number = int.Parse(Console.ReadLine());
+
+            int i = 1;
+
+            while (i <= 10)
+            {
+                Console.WriteLine(number + "X" + i + "=" + number*i);
+                i++;
+            }
+            Console.WriteLine("while loop terminated...");
+            Console.ReadLine();
+        }
+    }
+}
