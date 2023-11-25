2c4646964add294dcb35d35215e3ba375b5de296
Rutikkale <rutikkale17@gmail.com>
2023-11-25 15:12:23 +0530

    Nested for loop

---
+++ C#/Nested For Loop/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nested_For_Loop
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1; i <= 3; i++)
+            {
+                Console.WriteLine("Hello");
+                for (int j = 1; j <= 3; j++)
+                {
+                    Console.WriteLine("Hey");
+                }
+            }
+            Console.WriteLine("Loop Terminates...");
+            Console.ReadLine();
+        }
+    }
+}
