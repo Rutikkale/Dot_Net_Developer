0763e68437e19ab4104b744adcdd64a5ad2a7549
Rutikkale <rutikkale17@gmail.com>
2023-12-08 15:55:06 +0530

    Arrays

---
+++ C#/Array/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Array
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Example 1
+
+            //int[] My_arrays = new int[4];
+
+            //My_arrays[0] = 11;
+            //My_arrays[1] = 22;
+            //My_arrays[2] = 33;
+            //My_arrays[3] = 44;
+
+            //Console.WriteLine(My_arrays[0]);
+            //Console.WriteLine(My_arrays[1]);
+            //Console.WriteLine(My_arrays[2]);
+            //Console.WriteLine(My_arrays[3]);
+
+            //Console.ReadLine();
+
+            // Example 2
+
+            //string[] My_arrays = new String[4];
+
+            //My_arrays[0] = "Rutik";
+            //My_arrays[1] = "Arpit";
+            //My_arrays[2] = "Harshal";
+            //My_arrays[3] = "Akshay";
+
+            //Console.WriteLine(My_arrays[0]);
+            //Console.WriteLine(My_arrays[1]);
+            //Console.WriteLine(My_arrays[2]);
+            //Console.WriteLine(My_arrays[3]);
+
+            //Console.ReadLine();
+
+            //Example 3
+            //string[] My_arrays = new String[] { "Rutik", "Arpit", "Harshal", "Akshay" };
+
+            //Console.WriteLine(My_arrays[0]);
+            //Console.WriteLine(My_arrays[1]);
+            //Console.WriteLine(My_arrays[2]);
+            //Console.WriteLine(My_arrays[3]);
+
+            //Console.ReadLine();
+
+
+            //Example 4
+
+            //string[] My_arrays1 = { "Rutik", "Arpit", "Harshal", "Akshay" };
+
+            //Console.WriteLine(My_arrays1[0]);
+            //Console.WriteLine(My_arrays1[1]);
+            //Console.WriteLine(My_arrays1[2]);
+            //Console.WriteLine(My_arrays1[3]);
+
+            //Console.ReadLine();
+
+            // Example 5
+            string[] My_arrays1 = { "Rutik", "Arpit", "Harshal", "Akshay" };
+
+            Console.WriteLine(My_arrays1.Length);
+            Console.ReadLine();
+        }
+    }
+}
