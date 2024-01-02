8cb518377824b7c9f8f7dfa2ab27aeaffd198ddf
Rutikkale <rutikkale17@gmail.com>
2024-01-02 20:50:18 +0530

    Value Type and Reference Type

---
+++ C#/Value Type Reference Type/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Value_Type_Reference_Type
+{
+  // struct -- Value type -- stack memory
+
+  /*  struct Employee
+    {
+        public int Salary;
+        public int Age;
+    }
+  */
+
+    // Class -- Reference Type -- Heap memory
+
+    class Employee
+    {
+        public int salary;
+        public int Age;
+
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // struct example 1
+
+           /* Employee e = new Employee();
+            e.Salary = 5000;
+            e.Age = 23;
+
+            Employee e1 = e;
+            Employee e2 = e;
+            e.Age = 25;
+
+            Console.WriteLine(e.Age);
+            Console.WriteLine(e1.Age);
+            Console.WriteLine(e2.Age);
+            Console.ReadLine();
+           */
+
+            // Class Example 2
+            Employee e = new Employee();
+            e.salary = 5000;
+            e.Age = 23;
+
+            Employee e1 = e;
+            Employee e2 = e;
+
+            e.Age = 25;
+            Console.WriteLine(e.Age);
+            Console.WriteLine(e1.Age);
+            Console.WriteLine(e2.Age);
+            Console.ReadLine();
+        }
+    }
+}
