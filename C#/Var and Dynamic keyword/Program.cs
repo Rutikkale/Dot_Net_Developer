44d155991bfdb0b6a68a10a6c81d5449b457bb85
Rutikkale <rutikkale17@gmail.com>
2024-01-10 14:40:15 +0530

    var and dynamic keyword

---
+++ C#/Var and Dynamic keyword/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Var_and_Dynamic_keyword
+{
+    internal class Program
+    {
+        // Example 1 of Var keyword
+
+        //public static var show(var a, var b) // we can not used var in method or funtion 
+        //{ 
+        //    return a + b;
+        //}
+
+        // Example 2 of Dynamic Keyword
+
+        public static void show(dynamic a)
+        {
+            Console.WriteLine(a);
+        }
+
+        //public static dynamic show(dynamic a)
+        //{
+        //    return a;
+        //}
+
+
+
+        static void Main(string[] args)
+        {
+            // Example 1 of var keyword
+
+            // var a = "Rutik";
+            //// a = 123;
+            // Console.WriteLine(a.GetType());
+            // Console.WriteLine(a.Length);
+            // Console.WriteLine(a.ToUpper());
+            // Console.ReadLine();
+
+            // Example 2 of Dynamic Keyword
+
+            //dynamic a;
+            //a = "Rutik";
+            //Console.WriteLine(a.GetType());
+            //Console.WriteLine(a.Length);
+            //Console.WriteLine(a.ToUpper());
+            //Console.ReadLine();
+
+            Program.show(1);
+            Program.show("Rutik");
+            Program.show(true);
+            Program.show(false);
+            Program.show(12.200);
+
+            Console.ReadLine();
+            
+
+
+        }
+    }
+}
