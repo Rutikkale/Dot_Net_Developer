4846ee333d491aa6897ca6298ffa002e34a0f153
Rutikkale <rutikkale17@gmail.com>
2024-02-19 17:38:58 +0530

    Private Constructor

---
+++ C#/Private Constructor/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Private_Constructor
+{
+    class Example
+    {
+        public string Name;
+        public string Fname;
+
+        public static int a;
+
+     private Example()
+      { 
+        
+      }
+
+        private Example(int a)
+        {
+
+        }
+
+        //public Example()
+        //{ }
+
+        public Example(string name,string Fname)
+        {
+            this.Name = name;
+            this.Fname = Fname;
+        }
+
+        //class Example2 :Example 
+        //{
+
+        //}
+
+        public static int GetIncrement()
+        {
+        return ++a;
+        }
+        public static void GetTime()
+        {
+            Console.WriteLine(DateTime.Now);
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Example e = new Example();
+            Example e = new Example("Rutik ","Kale");
+            Console.WriteLine(e.Name + e.Fname);
+
+            Example.GetTime();
+            Example.a = 23;
+            Console.WriteLine(Example.GetIncrement());
+            
+
+            
+            Console.ReadLine();
+        }
+    }
+}
