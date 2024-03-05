86b45777f715a62f412385108bce392d11ee0f66
Rutikkale <rutikkale17@gmail.com>
2024-03-05 18:31:01 +0530

    Windows Forms

---
+++ Windows Forms/Login Form/Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Login_Form
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // label text message show
+            label3.Text = "Hello: " + textBox1.Text;
+          // label3.Text = textBox1.Text;
+            label3.Visible = true;
+
+
+            // Pop up MessageBox
+            //MessageBox.Show("Hello:" + textBox1.Text);
+            //MessageBox.Show("Welcome to windows forms !!");
+        }
+    }
+}
