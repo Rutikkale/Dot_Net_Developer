f64e581fef8df73f64a5a64c667dde818380157a
Rutikkale <rutikkale17@gmail.com>
2024-03-19 15:42:15 +0530

    Single_Inheritance

---
+++ C#/Single_Inheritance/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Single_Inheritance
+{
+    class PermanentEmployees : Employees // Child class or Derived class of Employees
+    {
+        int PermanentSalary;
+        int PermanentHours;
+    }
+    class Employees // Base Class or Parents class
+    {
+        public int EmpId;
+        public string EmpName;
+        public int EmpAge;
+        public int EmpContactNo;
+
+        public void show()
+        {
+            Console.WriteLine("This is a method of base class !!");
+        }
+    }
+
+        internal class Program
+    {
+        static void Main(string[] args)
+        {
+           PermanentEmployees Rutik = new PermanentEmployees();
+            Rutik.EmpAge = 23;
+            Rutik.show();
+            Console.WriteLine( Rutik.EmpAge);
+            Console.ReadLine();
+        }
+    }
+}
