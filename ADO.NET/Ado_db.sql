55d0d2f923090375221bffd1ab4f5a89620c4771
Rutikkale <rutikkale17@gmail.com>
2024-04-27 11:59:10 +0530

    sqlconnection by connection string

--- ADO.NET/Ado_db.sql
+++ ADO.NET/Ado_db.sql
@@ -1,1 +1,17 @@
 use Ado_db
+
+create table employee_tbl(id int primary key,
+                          name varchar(50) not null,
+						  gender varchar(10) not null,
+						  age int not null,
+						  salary int not null,
+						  city varchar(50));
+
+select*from employee_tbl
+
+insert into employee_tbl values(1,'Rutik Kale','Male',23,25000,'Nagpur')
+insert into employee_tbl values(2,'Arpit Kale','Male',23,25000,'Nagpur')
+insert into employee_tbl values(3,'Harshal Shinde','Male',21,35000,'Mumbai')
+insert into employee_tbl values(4,'Ram Raut','Male',28,25000,'Yavatmal')
+insert into employee_tbl values(5,'Vikas Bankar','Male',35,45000,'Yavatmal')
+insert into employee_tbl values(6,'Sakshi Swarkar','Female',22,25000,'Nagpur')
---
+++ ADO.NET/ConnectionADO/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+
+
+namespace ConnectionADO
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Program.connection();
+            Console.ReadLine();
+        }
+        
+         static void connection()
+        {
+            string cs = "Data Source = LAPTOP-0BJDO9D1\\SQLEXPRESS ; Initial Catalog = Ado_db; Integrated Security = true ";
+            SqlConnection conn = null;
+
+            try
+            {
+                using (conn = new SqlConnection(cs))
+                {
+                    conn.Open();
+                    if (conn.State == ConnectionState.Open)
+                    {
+                        Console.WriteLine("connection has been Created Successfully!!");
+                    }
+                }
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+
+            }
+            finally
+            {
+                conn?.Close();
+            }
+
+        }
+    }
+
+}
