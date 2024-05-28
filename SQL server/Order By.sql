06380cf4fdfaade8f62f199af1a74be0fecb6803
Rutikkale <rutikkale17@gmail.com>
2024-05-28 18:09:57 +0530

    Order By

---
+++ SQL server/Order By.sql
@@ -0,0 +1,13 @@
+use Batch_01_2024;
+
+select*from Employee_Details;
+
+---------Order By ascending and Desending----------
+select*from Employee_Details
+order by Name asc;
+
+select*from Employee_Details
+order by Name desc;
+
+select*from Employee_Details
+order by Name desc, Gender asc;
