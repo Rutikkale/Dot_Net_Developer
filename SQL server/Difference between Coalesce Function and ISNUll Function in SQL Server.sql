498c15d21ffac67c423f7542aa93f39494bfbe41
Rutikkale <rutikkale17@gmail.com>
2024-06-21 16:04:59 +0530

    Coalesce function vs IsNull Function in SQL Server

---
+++ SQL server/Difference between Coalesce Function and ISNUll Function in SQL Server.sql
@@ -0,0 +1,18 @@
+use [Practice ];
+
+create table FullNameTbl(Id int primary key identity,
+                         FirstName varchar(50),
+                         MiddleName varchar(50),
+						 LastName Varchar(50));
+
+drop table FullNameTbl;
+
+insert into FullNameTbl(MiddleName) values('Kisanrao');
+insert into FullNameTbl(LastName) values('Kale');
+insert into FullNameTbl(FirstName) values('Arpit');
+insert into FullNameTbl(MiddleName) values('Kisanrao');
+
+select*from FullNameTbl;
+select Id, coalesce(FirstName,MiddleName,LastName) from FullNameTbl;
+
+select Id, ISNULL(FirstName,MiddleName) from FullNameTbl;
