db1d3085fce20fed2e40d792d5008e29c73c6e0c
Rutikkale <rutikkale17@gmail.com>
2024-09-24 22:15:51 +0530

    Rang Validator

---
+++ ASP.NET Web Forms/Rang Validator/RangValidator.aspx.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Rang_Validator
+{
+    public partial class RangValidator : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void UxSubmit_Click(object sender, EventArgs e)
+        {
+            Page.ClientScript.RegisterStartupScript(this.GetType(), "scriptKey", "<script>alert('You From has been submited succesfully!');</script>");
+
+        }
+    }
+}
