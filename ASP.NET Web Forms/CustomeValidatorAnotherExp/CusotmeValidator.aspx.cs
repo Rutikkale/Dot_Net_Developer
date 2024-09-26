7bf2224f1fcb51869a5cf3999f6015977859ec54
Rutikkale <rutikkale17@gmail.com>
2024-09-26 08:50:03 +0530

    Validation custome

---
+++ ASP.NET Web Forms/CustomeValidatorAnotherExp/CusotmeValidator.aspx.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace CustomeValidatorAnotherExp
+{
+    public partial class CusotmeValidator : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void UxSubmit_Click(object sender, EventArgs e)
+        {
+            Page.ClientScript.RegisterStartupScript(this.GetType(), "scriptKey", "<script>alert('You From has been submited succesfully!');</script>");
+        }
+
+        // Server-side custom validation for Gender
+        protected void ValidateGender(object source, ServerValidateEventArgs args)
+        {
+            // Ensure at least one radio button is selected (Male or Female)
+            if (MaleRadioButton.Checked || FemaleRadioButton.Checked)
+            {
+                args.IsValid = true;
+            }
+            else
+            {
+                args.IsValid = false;
+            }
+        }
+        
+    }
+}
---
+++ ASP.NET Web Forms/CustomeValidatorAnotherExp/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="CustomeValidatorAnotherExp.Global" Language="C#" %>
