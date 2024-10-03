7a48c7ff068e3cda59677256ad44728b59c00ec7
Rutikkale <rutikkale17@gmail.com>
2024-10-03 21:59:14 +0530

    Session State

---
+++ ASP.NET Web Forms/Session State/Session State/Session State/WebForm3.aspx
@@ -0,0 +1,17 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm3.aspx.cs" Inherits="Session_State.WebForm3" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    
+    <form id="form1" runat="server">
+        <div>
+<asp:Button ID="LogoutButton" runat="server" Text="Logout" OnClick="LogoutButton_Click" style="width: 57px" />
+        </div>
+    </form>
+</body>
+</html>
---
+++ ASP.NET Web Forms/Session State/Session State/Session State/WebForm3.aspx.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Session_State
+{
+    public partial class WebForm3 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (Session["User"] != null)
+            {
+                Response.Write("Welcome " + Session["User"].ToString());
+            }
+            else
+            {
+                Response.Redirect("Webform1.aspx");
+            }
+        }
+
+        protected void LogoutButton_Click(object sender, EventArgs e)
+        {
+            if(Session["User"] != null)
+            {
+                Session["User"] = null;
+                Response.Redirect("Webform1.aspx");
+            }
+        }
+    }
+}
