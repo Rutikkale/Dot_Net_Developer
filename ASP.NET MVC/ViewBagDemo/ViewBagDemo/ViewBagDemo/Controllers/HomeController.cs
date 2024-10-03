b9453bb91e81a10e655fc2ecbfaef16688832c96
Rutikkale <rutikkale17@gmail.com>
2024-10-03 21:24:30 +0530

    ViewBag

---
+++ ASP.NET MVC/ViewBagDemo/ViewBagDemo/ViewBagDemo/Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ViewBagDemo.Models;
+
+namespace ViewBagDemo.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            ViewBag.Message = "Message From View Bag !!";
+            ViewBag.CurrentDate = DateTime.Now.ToLongDateString();
+
+            string[] fruits = { "Apple", " Orange" };
+            ViewBag.FruitsArray = fruits;
+
+
+            ViewBag.SportsList = new List<string>()
+            {
+                "Cricket",
+                "Backetball",
+                "Hockey",
+                "BaseBall"
+            };
+
+            Employee Rutik = new Employee();
+
+            Rutik.Empid = 1;
+            Rutik.EmpName = "Rutik";
+            Rutik.EmpDesignation = "Dot Net Developer";
+            ViewBag.Employee = Rutik;
+
+
+            ViewBag.CommonMessage = "This message is accessible by both ViewBag and ViewData!!";
+
+            return View();
+        }
+    }
+}
---
+++ ASP.NET MVC/ViewBagDemo/ViewBagDemo/ViewBagDemo/Models/Employee.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ViewBagDemo.Models
+{
+    public class Employee
+    {
+        public int Empid { get; set; }
+
+        public string EmpName { get; set; }
+
+        public string EmpDesignation { get; set; }
+
+    }
+}
