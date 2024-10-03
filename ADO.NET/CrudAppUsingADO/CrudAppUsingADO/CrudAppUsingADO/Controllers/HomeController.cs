fbdf00d0b0e6f83a93966c4aba78333ad88638e4
Rutikkale <rutikkale17@gmail.com>
2024-10-03 21:31:28 +0530

    CrudAppUsingAdoNet

---
+++ ADO.NET/CrudAppUsingADO/CrudAppUsingADO/CrudAppUsingADO/Controllers/HomeController.cs
@@ -0,0 +1,103 @@
+﻿using CrudAppUsingADO.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CrudAppUsingADO.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            employeeDBContext db = new employeeDBContext();
+            List<employee> obj = db.GetEmployees();
+
+            return View(obj);
+        }
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(employee emp)
+        {
+            try
+            {
+
+                if (ModelState.IsValid == true)
+                {
+                    employeeDBContext context = new employeeDBContext();
+                    bool check = context.AddEmployee(emp);
+                    if (check == true)
+                    {
+                        TempData["InsertMessage"] = "Data has been Inserted Successfully !!!";
+                        ModelState.Clear();
+                        return RedirectToAction("Index");
+                    }
+
+                }
+                return View();
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        public ActionResult Edit( int id)
+        {
+            employeeDBContext context = new employeeDBContext();
+            var row = context.GetEmployees().Find(model => model.id == id);
+            return View(row);
+        }
+
+        [HttpPost]
+        public ActionResult Edit(int id,employee emp)
+        {
+            if (ModelState.IsValid == true)
+            {
+                employeeDBContext context = new employeeDBContext();
+                bool check = context.UpdateEmployee(emp);
+                if (check == true)
+                {
+                    TempData["UpdateMessage"] = "Data has been updated Successfully !!!";
+                    ModelState.Clear();
+                    return RedirectToAction("Index");
+                }
+
+            }
+            return View();
+        }
+
+        public ActionResult Details(int id)
+        {
+            employeeDBContext context = new employeeDBContext();
+            var row = context.GetEmployees().Find(model => model.id == id);
+            return View(row);
+        }
+        public ActionResult Delete(int id)
+        {
+            employeeDBContext context = new employeeDBContext();
+            var row = context.GetEmployees().Find(model => model.id == id);
+            return View(row);
+        }
+
+        [HttpPost]
+        public ActionResult Delete(int id, employee emp)
+        {
+            employeeDBContext context = new employeeDBContext();
+            bool check = context.DeleteEmployee(id);
+            if (check == true)
+            {
+                TempData["DeleteMessage"] = "Data has been delete Successfully !!!";
+                ModelState.Clear();
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+    }
+}
---
+++ ADO.NET/CrudAppUsingADO/CrudAppUsingADO/CrudAppUsingADO/Models/employeeDBContext.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.SqlClient;
+using System.Data;
+using System.Configuration;
+
+namespace CrudAppUsingADO.Models
+{
+    public class employeeDBContext
+    {
+        string cs = ConfigurationManager.ConnectionStrings["dbms"].ConnectionString;
+
+        public List<employee> GetEmployees()
+        {
+            List<employee> EmployeesList = new List<employee>();
+            SqlConnection con = new SqlConnection(cs);
+            SqlCommand cmd = new SqlCommand("spgetemployeeDetails", con);
+            cmd.CommandType = CommandType.StoredProcedure;
+            con.Open();
+            SqlDataReader dr = cmd.ExecuteReader();
+            while(dr.Read())
+            {
+                employee emp = new employee();
+                emp.id =Convert.ToInt32( dr.GetValue(0).ToString());
+                emp.name = dr.GetValue(1).ToString();
+                emp.gender = dr.GetValue(2).ToString();
+                emp.age = Convert.ToInt32( dr.GetValue(3).ToString());
+                emp.salary = Convert.ToInt32(dr.GetValue(4).ToString());
+                emp.city = dr.GetValue(5).ToString();
+                EmployeesList.Add(emp);
+            }
+
+            con.Close();
+
+            return EmployeesList;
+        }
+
+        public bool AddEmployee(employee emp)
+        {
+            SqlConnection con = new SqlConnection(cs);
+            SqlCommand cmd = new SqlCommand("spAddemployee", con);
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.Parameters.AddWithValue("@name", emp.name);
+            cmd.Parameters.AddWithValue("@gender", emp.gender);
+            cmd.Parameters.AddWithValue("@age", emp.age);
+            cmd.Parameters.AddWithValue("@salary", emp.salary);
+            cmd.Parameters.AddWithValue("@city", emp.city);
+           
+            con.Open();
+            int i= cmd.ExecuteNonQuery();
+            con.Close();
+            
+            if(i>0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public bool UpdateEmployee(employee emp)
+        {
+            SqlConnection con = new SqlConnection(cs);
+            SqlCommand cmd = new SqlCommand("spUpdateemployee", con);
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.Parameters.AddWithValue("@id", emp.id);
+            cmd.Parameters.AddWithValue("@name", emp.name);
+            cmd.Parameters.AddWithValue("@gender", emp.gender);
+            cmd.Parameters.AddWithValue("@age", emp.age);
+            cmd.Parameters.AddWithValue("@salary", emp.salary);
+            cmd.Parameters.AddWithValue("@city", emp.city);
+
+            con.Open();
+            int i = cmd.ExecuteNonQuery();
+            con.Close();
+
+            if (i > 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public bool DeleteEmployee(int id)
+        {
+            SqlConnection con = new SqlConnection(cs);
+            SqlCommand cmd = new SqlCommand("spDeleteemployee", con);
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.Parameters.AddWithValue("@id", id);
+            con.Open();
+            int i = cmd.ExecuteNonQuery();
+            con.Close();
+
+            if (i > 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
